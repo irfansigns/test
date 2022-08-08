a3f9f69aeef7632df4f8b40dd0db698cc81faa49
Irfan DellNew <irfansigns@gmail.com>
2022-08-08 16:41:52 +0500

    Reset and got deleted files again

---
+++ storage/framework/views/81c3429e22a0a83161200887275d0cc7165f11cf.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Forget Password </title>
+</head>
+<body>
+HI <br>
+
+ Change Your Password <a href="http://localhost:3000/reset/<?php echo e($data); ?>">Click Here</a>
+ <br>
+ Pincode : <?php echo e($data); ?>
+
+
+
+</body>
+</html><?php /**PATH D:\xampp\htdocs\reactcommerce\server\resources\views/mail/forgot.blade.php ENDPATH**/ ?>
